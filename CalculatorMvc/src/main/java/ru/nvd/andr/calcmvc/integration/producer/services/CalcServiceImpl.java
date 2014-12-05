a9622607a5d0769e99b949e7acc2102f7438c6c0
smith200791 <macbookretina@172.20.10.2>
2014-12-05 11:05:14 +0400

    RTKCRM-11. сделать интеграцию по средством SPRING WS - IN PROGRESS

--- CalculatorMvc/src/main/java/ru/nvd/andr/calcmvc/integration/producer/services/CalcServiceImpl.java
+++ CalculatorMvc/src/main/java/ru/nvd/andr/calcmvc/integration/producer/services/CalcServiceImpl.java
@@ -4,16 +4,27 @@ import org.springframework.stereotype.Service;
 
 import ru.nvd.andr.calcmvc.integration.producer.gen.ExecuteOperRequest;
 import ru.nvd.andr.calcmvc.integration.producer.gen.ExecuteOperResponse;
+import ru.nvd.andr.calcmvc.operations.Operation;
+import ru.nvd.andr.calcmvc.operations.OperationFactory;
 
 @Service
 public class CalcServiceImpl implements CalcService {
 
     public ExecuteOperResponse executeOper(ExecuteOperRequest executeOperRequest) {
-        
+        String operationCode = executeOperRequest.getOperation();
+        String firstArg = executeOperRequest.getFirstArg();
+        String secondArg = executeOperRequest.getSecondArg();
+
+        OperationFactory operationFactory = new OperationFactory();
+        Operation operation = operationFactory.createOperation(operationCode);
+        Long result = operation.excecute(Long.parseLong(firstArg), Long.parseLong(secondArg));
+
         ExecuteOperResponse executeOperResponse = new ExecuteOperResponse();
-        executeOperResponse.setResult("123");
+
+        executeOperResponse.setResult(result.toString());
+
         return executeOperResponse;
-        
+
     }
 
 }
